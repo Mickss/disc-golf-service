63e39dbfb87f161fcd1e6e172accfc079479c8b0
Michał Guz <michal.guz.85@gmail.com>
2023-11-12 21:21:35 +0100

    Initial commit

---
+++ src/test/java/org/micks/DiscGolfApplication/DiscGolfApplicationTests.java
@@ -0,0 +1,13 @@
+package org.micks.DiscGolfApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscGolfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
