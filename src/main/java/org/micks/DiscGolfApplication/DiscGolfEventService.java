c92b853254ff1d03ba2abc5012dc89f2f52c03e9
Michał Guz <michal.guz.85@gmail.com>
2024-01-13 15:34:04 +0100

    Creating events service

---
+++ src/main/java/org/micks/DiscGolfApplication/DiscGolfEventService.java
@@ -0,0 +1,19 @@
+package org.micks.DiscGolfApplication;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DiscGolfEventService {
+
+    public List<DiscGolfEventDTO> getEvents() {
+        return List.of(
+                new DiscGolfEventDTO("05/03/2024", "159", "-", "Fire Disk", "Phase 1", "22 / 22"),
+                new DiscGolfEventDTO("15/06/2024", "237", "-", "Spin Around", "Phase 1", "0 / 30"),
+                new DiscGolfEventDTO("04/08/2024", "262", "-", "Golf Basket", "Phase 1", "33 / 45"),
+                new DiscGolfEventDTO("09/10/2024", "305", "-", "Flaying plates", "Phase 1", "20 / 25"),
+                new DiscGolfEventDTO("11/11/2024", "356", "-", "Team Disk", "Phase 1", "22 / 26")
+        );
+    }
+}
