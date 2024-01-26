2a4a28b45085e41adbf8f0dc7768b688103b033b
Michał Guz <michal.guz.85@gmail.com>
2024-01-26 21:14:37 +0100

    Add configuration prepare statement

--- src/main/java/org/micks/DiscGolfApplication/DiscGolfEventService.java
+++ src/main/java/org/micks/DiscGolfApplication/DiscGolfEventService.java
@@ -4,6 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -15,6 +18,9 @@ public class DiscGolfEventService {
 
     @Autowired
     private DiscGolfDbConnection dbConnection;
+
+    @Autowired
+    private DatabaseConfigProperties databaseConfigProperties;
 
     public List<DiscGolfEventDTO> getEvents() {
         try (ResultSet resultSet = dbConnection.executeQuery("SELECT * FROM Events")) {
@@ -38,5 +44,25 @@ public class DiscGolfEventService {
 
     public void createEvent(DiscGolfEventDTO discGolfEventDTO) {
         log.info("Creating new event: {}", discGolfEventDTO.getTournamentTitle());
+        try {
+            String url = String.format("jdbc:mariadb://app.disc-golf.pl:3306/%s?user=%s&password=%s",
+                    databaseConfigProperties.getName(),
+                    databaseConfigProperties.getUser(),
+                    databaseConfigProperties.getPassword()
+            );
+            Connection connection = DriverManager.getConnection(url);
+
+            PreparedStatement statement = connection.prepareStatement("insert into emp values(?,?,?,?,?,?)");
+            statement.setString(1, discGolfEventDTO.getTournamentDate());
+            statement.setString(2, discGolfEventDTO.getPdga());
+            statement.setString(3, discGolfEventDTO.getTournamentTitle());
+            statement.setString(4, discGolfEventDTO.getRegion());
+            statement.setString(5, discGolfEventDTO.getRegistration());
+            statement.setString(6, discGolfEventDTO.getVacancies());
+            statement.execute();
+            statement.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
