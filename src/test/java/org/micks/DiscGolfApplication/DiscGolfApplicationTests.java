5b51160cdcb2bfdd9beb42e1ed5b91a6ed787ef2
Tomasz Nowak <nowaktomasz2@gmail.com>
2024-01-26 20:18:35 +0100

    Change database configuration

--- src/test/java/org/micks/DiscGolfApplication/DiscGolfApplicationTests.java
+++ src/test/java/org/micks/DiscGolfApplication/DiscGolfApplicationTests.java
@@ -3,7 +3,7 @@ package org.micks.DiscGolfApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "db.name=mock-db")
 class DiscGolfApplicationTests {
 
 	@Test
