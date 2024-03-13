cf478c2018007e346bc34038eb7e9ea4681dab15
Tomasz Nowak <nowaktomasz2@gmail.com>
2024-03-13 20:19:39 +0100

    Change tournamentDate column type from string to date

--- src/main/java/org/micks/DiscGolfApplication/DiscGolfEventDTO.java
+++ src/main/java/org/micks/DiscGolfApplication/DiscGolfEventDTO.java
@@ -4,12 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
+import java.util.Date;
+
 @Getter
 @AllArgsConstructor
 @ToString
 public class DiscGolfEventDTO {
     private String id;
-    private String tournamentDate;
+    private Date tournamentDate;
     private String pdga;
     private String tournamentTitle;
     private String region;
