a51d0a2d766166cdd1ad32c679c34beeb0da1e34
Michał Guz <michal.guz.85@gmail.com>
2026-02-11 15:02:51 +0100

    DG-217 Add diagnostic recorders to the authentication process.

--- src/main/java/org/micks/DiscGolfApplication/events/PublicEventsController.java
+++ src/main/java/org/micks/DiscGolfApplication/events/PublicEventsController.java
@@ -1,10 +1,12 @@
 package org.micks.DiscGolfApplication.events;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/public")
 public class PublicEventsController {
@@ -15,6 +17,7 @@ public class PublicEventsController {
     @GetMapping("/events")
     public List<DiscGolfEventDTO> getEvents(@RequestParam(required = false) String valueToOrderBy,
                                             @RequestParam(required = false) OrderDirection orderDirection) {
+        log.info("Public events: {}, {} ", valueToOrderBy, orderDirection);
         return discGolfEventService.getEvents(valueToOrderBy, orderDirection);
     }
 
